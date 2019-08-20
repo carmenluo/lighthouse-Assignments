349b99e3332a116cb9d3293c7767564d3472eb7d
Carmen <sdluojm@gmail.com>
2019-08-20 21:02:36 +0000

    add assertion

--- lotide/without.js
+++ lotide/without.js
@@ -1,3 +1,20 @@
+const eqArrays = function(arr1 , arr2){
+  for (var i = 0 ; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length != arr2.length) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1 , arr2)) {
+      console.log("🙂🙂🙂🙂Assertion passed");
+  }
+  else {
+      console.log("😿😿😿Assertion failed");}
+}
+
 const without = function(source, itemToRemove) {
   let result = [];
   result = source.filter((elementSource) => {
@@ -16,3 +33,7 @@ const without = function(source, itemToRemove) {
 }
 console.log(without([1, 2, 3], [1,2]));
 console.log(without(["hello", "world", "lighthouse"], ["lighthouse"]));
+let words = ["hello", "world", "lighthouse"];
+words = without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
