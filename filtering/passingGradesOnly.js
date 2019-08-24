860388c6b24e968a6fd22d28ad3bc24e28f494f9
Carmen Luo <sdluojm@gmail.com>
2019-08-24 07:37:11 -0400

    Delete passingGradesOnly.js

--- filtering/passingGradesOnly.js
+++
@@ -1,5 +0,0 @@
-const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
-console.log(grades.filter(num => num >= 70));
-// console.log(grades.filter(function(num){
-//   return num >= 70;
-// }))
