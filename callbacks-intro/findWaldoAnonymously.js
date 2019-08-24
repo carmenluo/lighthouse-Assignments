29f1ac411f3f44d1310bfda4f1ab71c0498136e1
Carmen Luo <sdluojm@gmail.com>
2019-08-24 07:36:46 -0400

    Delete findWaldoAnonymously.js

--- callbacks-intro/findWaldoAnonymously.js
+++
@@ -1,7 +0,0 @@
-const findWaldo = function (names, found) {
-  names.forEach((value, i) => value === 'Waldo' ? found(i) : void 0 );
-}
-//findWaldo(['ALice','Bob', 'Waldo', 'Winston'], actionWhenFound);
-findWaldo(['Alice', 'Bob', 'Waldo', 'Winston'], function (index) {
-  console.log('Waldo is located at:', index);
-})
