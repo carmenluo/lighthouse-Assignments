e9f21ee701a48f9874195e37ac3e45ea5551743b
Carmen Luo <sdluojm@gmail.com>
2019-08-27 04:17:39 -0400

    used mocha and chai

--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,4 +1,10 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+//const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!  
+//assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!  
+describe("#tail", () => {
+  it(`returns ${result} for ["Lighthouse", "Labs"]`, () => {
+    assert.deepEqual(result, ["Lighthouse", "Labs"]);
+  });
+});
