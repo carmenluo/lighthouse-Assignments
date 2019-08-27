37f41ad5ed25ccdcc40614d8bebfc35214ee8f10
Carmen Luo <sdluojm@gmail.com>
2019-08-27 03:24:00 -0400

    export as modules

--- lotide/head.js
+++ lotide/head.js
@@ -1,10 +1,11 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🚀🚀🚀 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😢😢😢 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     console.log(`🚀🚀🚀 Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`😢😢😢 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+const assertEqual = require('./assertEqual');
 const head = function (arr) {
   if (arr == null) {
     return undefined;
@@ -14,10 +15,4 @@ const head = function (arr) {
   }
 }
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
-
-assertEqual(head([6]), 5);
-assertEqual(head([]), "Hello");  
+module.exports = head;
