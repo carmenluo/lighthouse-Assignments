0076a45b7753a44f7ee431d5a4db18e0f837967e
Carmen Luo <sdluojm@gmail.com>
2019-08-28 08:18:12 -0400

    update with recursive func

--- lotide/flatten.js
+++ lotide/flatten.js
@@ -1,30 +1,15 @@
-const eqArrays = function(arr1 , arr2){
-  for (var i = 0 ; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i] || arr1.length != arr2.length) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const asserArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1 , arr2)) {
-      console.log("🙂🙂🙂🙂Assertion passed");
-  }
-  else {
-      console.log("😿😿😿Assertion failed");}
-}
+const eqArrays = require('./eqArrays');
+const asserArraysEqual = require('./assertArraysEqual');
 const flatten = function(arr){
   let result = [];
   for (item of arr){
     if (Array.isArray(item)){
-      item.forEach(element => {result.push(element); 
-      });
+      let temp = flatten(item);
+      temp.forEach(item=>result.push(item));
     }
     else result.push(item);
   }
   return result;
 }
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-console.log(flatten([['hello','world'],['carmen']]));
-asserArraysEqual(['hello','world','carmen'],flatten([['hello','world'],['carmen']]))
+
+module.exports = flatten;
